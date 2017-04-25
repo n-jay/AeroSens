7256330b17afaceeac713cfbcce9c45a42ad1271
Nuwan Jayawardene <w1608508@my.westminster.ac.uk>
2017-04-25 21:09:49 +0530

    Added files
    Included all the source files of the app

---
+++ src/main/java/com/talejunky/aerosens2/BtConnect.java
@@ -0,0 +1,13 @@
+package com.talejunky.aerosens2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BtConnect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt_connect);
+    }
+}
---
+++ src/main/java/com/talejunky/aerosens2/MainActivity.java
@@ -0,0 +1,158 @@
+package com.talejunky.aerosens2;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //BELOW IS THE LOGIC FOR A BUTTON IN MAIN ACTIVITY SCREEN THAT I DISABLED IN XML FILE THUS I DISABLED THE LOGIC AS WELL
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Toast.makeText(this, "Home Screen", Toast.LENGTH_SHORT).show();
+
+            HomeFragment homeFragment = new HomeFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.main_layout, homeFragment).commit();
+
+        } else if (id == R.id.nav_analytics) {
+            Toast.makeText(this, "Air Analysis", Toast.LENGTH_SHORT).show();
+
+            AnalyticsFragment analyticsFragment = new AnalyticsFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.main_layout, analyticsFragment).commit();
+
+        } else if (id == R.id.nav_map) {
+            Toast.makeText(this, "Pollution Map", Toast.LENGTH_SHORT).show();
+
+            MapFragment mapFragment = new MapFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.main_layout, mapFragment).commit();
+
+        } else if (id == R.id.ic_history) {
+            Toast.makeText(this, "Exposure History", Toast.LENGTH_SHORT).show();
+
+            HistoryFragment historyFragment = new HistoryFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.main_layout, historyFragment).commit();
+        } else if (id == R.id.nav_share) {
+            Toast.makeText(this, "Share Data", Toast.LENGTH_SHORT).show();
+
+            ShareFragment shareFragment = new ShareFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.main_layout, shareFragment).commit();
+        } else if (id == R.id.nav_team) {
+            Toast.makeText(this, "Meet the Team", Toast.LENGTH_SHORT).show();
+
+            TeamFragment teamFragment = new TeamFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.main_layout, teamFragment).commit();
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    ImageButton floatButton;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        floatButton = (ImageButton) findViewById(R.id.imageButton);
+//        floatButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(),
+//                        "Button is clicked", Toast.LENGTH_LONG).show();
+//            }
+//        });
+//    }
+
+//    @Override
+//    public void onFragmentInteraction(Uri uri) {
+//
+//    }
+}
